ed198c2d2121301a2114ee6a14cc85c794241f38
Gregorius Soedharmo <arkatufus@yahoo.com>
2024-01-12 22:23:42 +0700

    Fix Akka.Analyzer AK2001 warnings

--- src/WebApiTemplate/src/WebApiTemplate.App/Actors/GenericChildPerEntityParent.cs
+++ src/WebApiTemplate/src/WebApiTemplate.App/Actors/GenericChildPerEntityParent.cs
@@ -13,27 +13,27 @@ public sealed class GenericChildPerEntityParent : ReceiveActor
 {
     public static Props Props(IMessageExtractor extractor, Func<string, Props> propsFactory)
     {
-        ExtractEntityId realExtractor = message => (extractor.EntityId(message), extractor.EntityMessage(message)); 
-        return Akka.Actor.Props.Create(() => new GenericChildPerEntityParent(realExtractor, propsFactory));
+        return Akka.Actor.Props.Create(() => new GenericChildPerEntityParent(extractor, propsFactory));
     }
     
     /*
      * Re-use Akka.Cluster.Sharding's infrastructure here to keep things simple.
      */
-    private ExtractEntityId _extractor;
+    private readonly IMessageExtractor _extractor;
     private Func<string, Props> _propsFactory;
 
-    public GenericChildPerEntityParent(ExtractEntityId extractor, Func<string, Props> propsFactory)
+    public GenericChildPerEntityParent(IMessageExtractor extractor, Func<string, Props> propsFactory)
     {
         _extractor = extractor;
         _propsFactory = propsFactory;
         
         ReceiveAny(o =>
         {
-            var result = extractor(o);
-            if (!result.HasValue) return;
-            var (id, msg) = result.Value;
-            Context.Child(id).GetOrElse(() => Context.ActorOf(propsFactory(id), id)).Forward(msg);
+            var entityId = _extractor.EntityId(o);
+            if (string.IsNullOrEmpty(entityId)) 
+                return;
+            Context.Child(entityId).GetOrElse(() => Context.ActorOf(propsFactory(entityId), entityId))
+                .Forward(_extractor.EntityMessage(o));
         });
     }
 }
