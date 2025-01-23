1c775630f4dfbe92d62066f884331ab4db66a8ec
Aaron Stannard <aaron@petabridge.com>
2025-01-22 20:34:31 -0600

    added `build.ps1` for release notes bumping

---
+++ build.ps1
@@ -0,0 +1,22 @@
+. "$PSScriptRoot\scripts\getReleaseNotes.ps1"
+. "$PSScriptRoot\scripts\bumpVersion.ps1"
+
+Set-StrictMode -Version latest
+$ErrorActionPreference = "Stop"
+
+######################################################################
+# Step 1: Grab release notes and update solution metadata
+######################################################################
+$releaseNotes = Get-ReleaseNotes -MarkdownFile (Join-Path -Path $PSScriptRoot -ChildPath "RELEASE_NOTES.md")
+
+# inject release notes into Directory.Buil
+UpdateVersionAndReleaseNotes -ReleaseNotesResult $releaseNotes -XmlFilePath (Join-Path -Path $PSScriptRoot -ChildPath "Akka.Templates.csproj") 
+
+Write-Output "Added release notes $releaseNotes"
+
+######################################################################
+# Step 2: Build output
+######################################################################
+$outputDir = ".\bin\nuget"
+
+dotnet pack -c Release -o $outputDir
