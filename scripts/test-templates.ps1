b2f988de4f64e71844462dfe9a62992a1185cb42
Aaron Stannard <aaron@petabridge.com>
2026-09-17 12:16:49 +0000

    Migrate to .slnx and keep .NET SDK container publishing
    - Replace WebApiTemplate.sln with WebApiTemplate.slnx (dotnet sln migrate);
      update template.json primaryOutputs and verify sourceName rename in generated apps.
    - Restore the .NET SDK Docker publishing capability (ContainerPort,
      ContainerRepository, DockerDefaultTargetOS) on the App project. The .NET 10
      SDK bundles Microsoft.NET.Build.Containers so no package pin is needed.
      Verified: 'dotnet publish -c Release -t:PublishContainer' builds and pushes
      webpapitemplate-app:{version},latest to the local registry.
    - Restore the Test-Template smoke-test (build + test + container publish) and
      use it for both WebApi discovery backends.

--- scripts/test-templates.ps1
+++ scripts/test-templates.ps1
@@ -35,6 +35,30 @@ function Exec
     }
 }
 
+function Test-Template {
+    param (
+        [Parameter(Position=0,Mandatory=1)][string]$template,
+        [Parameter(Position=1,Mandatory=1)][string]$name,
+        [Parameter(Position=2,Mandatory=1)][string]$lang,
+        [Parameter(Position=3,Mandatory=1)][string]$parameterName,
+        [Parameter(Position=4,Mandatory=1)][string]$value,
+        [Parameter(Position=5,Mandatory=0)][string]$bl
+    )
+
+    $folderName = $name + $parameterName + $value
+
+    # Remove dots and - from folderName because in sln it will cause errors when building project
+    $folderName = $folderName -replace "[.-]"
+
+    # Create the project
+    Exec { dotnet new $template -o output//$lang/$folderName -$parameterName $value -lang $lang }
+
+    # Build, test, and publish the container
+    Exec { dotnet build output/$lang/$folderName -bl:$bl }
+    Exec { dotnet test output/$lang/$folderName -bl:$bl }
+    Exec { dotnet publish -c Release -t:PublishContainer output/$lang/$folderName -bl:$bl }
+}
+
 function Create-And-Build {
     param (
         [Parameter(Position=0,Mandatory=1)][string]$template,
@@ -86,9 +110,9 @@ Create-And-Build "akka.streams" "AkkaStreams" "C#" "f" "net8.0" $binlog
 Create-And-Build "akka.streams" "AkkaStreams" "F#" "f" "net9.0" $binlog
 Create-And-Build "akka.streams" "AkkaStreams" "F#" "f" "net8.0" $binlog
 
-# WebApi template is .NET 10 / Aspire-only: no -f framework choice, no container publish.
-# Smoke-test both discovery backends.
-Create-And-Build "akka.cluster.webapi" "ClusterWebTemplate" "C#" "Discovery" "redis" $binlog
-Create-And-Build "akka.cluster.webapi" "ClusterWebTemplate" "C#" "Discovery" "azure" $binlog
+# WebApi template is .NET 10 / Aspire-only: no -f framework choice.
+# Smoke-test both discovery backends, including container publish.
+Test-Template "akka.cluster.webapi" "ClusterWebTemplate" "C#" "Discovery" "redis" $binlog
+Test-Template "akka.cluster.webapi" "ClusterWebTemplate" "C#" "Discovery" "azure" $binlog
 
 # Ignore errors when files are still used by another process
